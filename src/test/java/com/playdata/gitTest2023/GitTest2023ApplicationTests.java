8293ffda06cbeba2edd92e16675eb4f3b2d3006f
heaves <heaves@hanmail.net>
2023-10-22 22:34:55 +0900

    github에 저장소를 만들었습니다. fork해서 가져가세요

---
+++ src/test/java/com/playdata/gitTest2023/GitTest2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.gitTest2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
